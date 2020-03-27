07884dbbdcf9fcca8ffbcf7a783326ecc7cb7557
Emmanuel Augustine <emmanuelaugustine@hotmail.com>
2020-03-27 01:03:34 -0700

    Updates

---
+++ precache-manifest.3ec3177c31b4be251d7167c31a458723.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656232aaec9d4a07f40d8f1bdc098973",
+    "url": "/stat-counter-react-app/index.html"
+  },
+  {
+    "revision": "8182f85627b3b7ec1a18",
+    "url": "/stat-counter-react-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4378772acfc98c39885a",
+    "url": "/stat-counter-react-app/static/css/main.f7117cae.chunk.css"
+  },
+  {
+    "revision": "8182f85627b3b7ec1a18",
+    "url": "/stat-counter-react-app/static/js/2.cdf2c6be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stat-counter-react-app/static/js/2.cdf2c6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4378772acfc98c39885a",
+    "url": "/stat-counter-react-app/static/js/main.54150707.chunk.js"
+  },
+  {
+    "revision": "4d53f6819561f22a1976",
+    "url": "/stat-counter-react-app/static/js/runtime-main.b96c5811.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stat-counter-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
